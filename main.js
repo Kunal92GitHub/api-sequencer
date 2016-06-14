4033f201b7135abf5346ab87638f2f58970d99a7
prmohanty08 <prmohanty08@yahoo.co.in>
2016-06-14 07:49:32 +0530

    UI Draft
    Created the first draft of the UI

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false})
+  mainWindow = new BrowserWindow({width: 1000, height: 600, frame: false})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
